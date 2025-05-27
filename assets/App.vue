ad927b57f32b581b4b434d262c35cc0947eeb85c
EdmonJiang <edmon.jiang@gmail.com>
2025-05-27 14:45:06 +0800

    color

--- assets/App.vue
+++ assets/App.vue
@@ -3,7 +3,7 @@
       @dragenter.prevent 
       @dragover.prevent 
       @drop.prevent="onDrop"
-      :style="{ background: burlywood }"
+      :style="{ background: "burlywood" }"
   >
     <progress v-if="uploadProgress !== null" :value="uploadProgress" max="100"></progress>
     <UploadPopup v-model="showUploadPopup" @upload="onUploadClicked" @createFolder="createFolder"></UploadPopup>
